7de647f57e5a5ce90fee5a0fc60498a3496d1ce2
Bryam Vicente <vicentebryam@gmail.com>
2020-08-09 19:43:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,8 +5,8 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  result = num2 - num1
-  puts "#{num2} - #{num1}"
+  result = num1 - num2
+  puts "#{num1} - #{num2}"
   return result
 end
 
